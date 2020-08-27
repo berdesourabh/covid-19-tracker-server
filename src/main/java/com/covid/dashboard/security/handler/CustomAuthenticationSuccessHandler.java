02ebb6ed798d65cd4345060b7601e13515a05d5d
saurabh <saurabhbhagat@gmail.com>
2020-08-27 19:31:39 +0530

    Spring Security Integration

---
+++ src/main/java/com/covid/dashboard/security/handler/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.covid.dashboard.security.handler;
+
+import com.covid.dashboard.security.dto.LoginSuccessResponse;
+import com.covid.dashboard.security.dto.UserDetailInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetailInfo userDetailInfo = (UserDetailInfo)authentication.getPrincipal();
+        response.getOutputStream().println(new ObjectMapper().writeValueAsString(userDetailInfo));
+    }
+}
